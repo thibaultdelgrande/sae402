e118425b5e2aa563e319cc0d83f901496f5c7ab1
Thibault Delgrande <thib@ultdelgran.de>
2023-04-14 03:06:01 +0200

    🚧WIP🚧

--- statue/script.js
+++ statue/script.js
@@ -27,6 +27,12 @@ let tableau = {
 }
 
 function AffichageTableau() {
+    // Si le tableau est dans l'odre, on affiche un message de victoire
+    if (tableau.case.every((element, index) => element.id === index)) {
+        document.querySelector("#win").style.display = "flex";
+        return;
+    }
+
     // Affichage du tableau
     document.querySelector("#tableau").innerHTML = "";
     tableau.case.forEach(function (element) {
@@ -35,17 +41,27 @@ function AffichageTableau() {
         image.dataset.id = element.id;
         image.rotation = element.rotation;
         image.style.transform = "rotate(" + image.rotation + "deg)";
-        image.addEventListener("click", rotation);
+        image.addEventListener("click", clicked);
         document.querySelector("#tableau").appendChild(image);
     });
+
+    // Créer les roues sur lesquelles on peut cliquer pour faire tourner les cases
+    for (let i = 0; i < tableau.width; i++) {
+            let roue = document.createElement("img");
+            roue.src = "img/roue.svg";
+            roue.dataset.id = i;
+            roue.addEventListener("click", clicked);
+            document.querySelector("#roues").appendChild(roue);
+    }
 }
 
-AffichageTableau();
-
 // position_case correspond à la position de la case en haut à gauche
-function rotation() {
+function clicked() {
     position_case= Array.prototype.indexOf.call(this.parentNode.children, this);
+    rotate(position_case);
+}
 
+function rotate(position_case){
     // Vérifie si la case est sur la ligne droite ou la ligne du bas du tableau
     if (position_case % tableau.width == tableau.width - 1 || position_case >= tableau.width * (tableau.height - 1)) {
         return;
@@ -74,7 +90,20 @@ function rotation() {
     tableau.case[position_case + tableau.width] = cases[2];
     tableau.case[position_case] = cases[3];
 
-    console.log(tableau);
-
     AffichageTableau();
 }
+
+
+// Mélange le tableau
+
+function melange() {
+    for (let i = 0; i < 25; i++) {
+        let position_case = Math.floor(Math.random() * (tableau.width * tableau.height - 1));
+        rotate(position_case);
+    }
+}
+
+
+melange();
+
+AffichageTableau();
